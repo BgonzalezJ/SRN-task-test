1bedc24cf61fd73f48e82f195766a33777dbab8a
Bastian Gonzalez <bgonzalezjara@gmail.com>
2025-08-25 02:56:35 -0400

    Se agregan las rutas que apuntan a las funciones de TasksController con los verbos http correspondientes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,12 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'TasksController::home');
+
+$routes->group('tasks', static function($routes) {
+    $routes->get('/', 'TasksController::index');
+    $routes->get('(:num)', 'TasksController::show/$1');
+    $routes->post('/', 'TasksController::create');
+    $routes->put('(:num)', 'TasksController::update/$1');
+    $routes->delete('(:num)', 'TasksController::delete/$1');
+});
